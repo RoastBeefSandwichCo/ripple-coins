f9d44be4ab6017a3c1d9a19d8d3309156b8b76ff
ninobrooks <ninobrooks@gmail.com>
2014-06-28 17:23:32 -0500

    Update rest-comms.js

--- rest-comms.js
+++ rest-comms.js
@@ -7,28 +7,19 @@
 
 
 var Client = require('node-rest-client').Client;
-
-
 client = new Client();
-
 args ={
         path:{"id":120}, // will be either pending_withdrawals or deposits. later. unused right now.
         parameters:{arg1:"hello",arg2:"world"}, // query parameter substitution vars
         headers:{"Accepts":"application/json"} // request headers
       };
 
-
-
-
-
 //use later during withdrawal testing.
 //client.registerMethod("jsonMethod", "http://remote.site/rest/json/${id}/method", "GET");
 client.registerMethod("jsonMethod", "http://localhost:5990/v1", "GET");
 
+//QUERY API
 client.methods.jsonMethod(args,function(data,response){
-    // parsed response body as js object
-    //console.log(data);
-    //data is still a string. make object.
     jsData = JSON.parse(data);
     console.log(jsData);
     console.log('---------------------\n\n');
@@ -36,10 +27,7 @@ client.methods.jsonMethod(args,function(data,response){
     console.log('data.endpoints.server_connected :', jsData.endpoints.server_connected);
 });
 
-    
-    
-    // raw response
-    //console.log(response);
+
 
 //client.get("http://remote.site/rest/json/${id}/method?arg1=hello&arg2=world", args, 
 /*client.get("http://localhost:5990/v1", args, 
