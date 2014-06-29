66c2bfe7e363e9d9e7020596e428d116ac338baa
ninobrooks <ninobrooks@gmail.com>
2014-06-28 20:32:25 -0500

    Update ledger-listener.js
    Removed large chunk of unused, commented code.

--- ledger-listener.js
+++ ledger-listener.js
@@ -1,17 +1,6 @@
 /*This module creates a websocket to listen to the ripple network
 It watches for ledger_closes to trigger queries to the REST api for pending deposit or withdrawal
-
-
-//old intro
-/*This module creates a websocket to listen to the ripple network
-It watches for transactions that may indicate a pending deposit or withdrawal
-It is NOT meant to fully examine them, just to signal when it's probably
-a good idea to check the api for a pending deposit or withdrawal.
-An event-based model instead of polling the api. */
-
-//It may have other uses.
-
-//I just can NOT get subscribe to work so I use request.on
+*/
 
 require ("console").log;
 
@@ -46,59 +35,3 @@ request.on('ledger_closed', function(res){
     //CODE GOES HERE
     //QUERY REST API v1/pending_withdrawals
 });
-
-//After some discussion with Artur Britto, I've opted to just use ledger_close as a trigger for polling the rest api
-//The code below, While more comprehensive, really isn't necessary.
-/*
-var withdrawalCount = 0;
-
-accountsRock = 'rLEsXccBGNR3UPuPu2hUXPjziKC3qKSBun'; // a couple of addresses to watch
-accountsMe = 'rPyBms1XZtNbF4UFGgM1dDWTmtfDmsfGNs';
-request.on('transaction_all', function(res){ //on any transaction in the ripple network (for testing. Not for production)
-//Show some basic info on transactions we see
-console.log('(', withdrawalCount,')', res.transaction.TransactionType,' : ',res.transaction.Account, ' : ', res.engine_result , ' : ' , res.engine_result_message);
-
-if (res.transaction.TransactionType === 'Payment'){ //Sorting for these
-    if (res.transaction.Amount.issuer == res.transaction.Destination){ //Withdrawals, right?
-        console.log('WITHDRAWAL!!', res);
-        withdrawalCount++;
-        }
-
-    console.log('--------------------\nPAYMENT!\n----'); // Mention all payments in case I'm wrong about withdrawals. I probably am.
-    console.log('Account:',res.transaction.Account);
-    console.log('Destination:', res.transaction.Destination);
-    console.log('Issuer:', res.transaction.Amount.issuer)
-;
-    if (res.engine_result === 'tesSUCCESS'){ // Show only successful finalized payments fully
-        console.log('~~~~\nGOOD?\n~~~~\n', res);
-    }
-    else{
-        //display basic info about everything we see. Just cause.
-        console.log('engine_result:',res.engine_result,'\nengine_result_message:', res.engine_result_message,'\n');
-    };
-    console.log('-----------------------');
-
-}
-else{
-//console.log('not a payment');
-//console.log('type: ' + res.type);
-};
-;
-if(res.Account === accountsRock){
-console.log('THATS THE ROCK!');
-};
-
-if(res.Account === accountsMe){
-console.log('THATS ME!');
-};
-
-
-});
-*/
-/*
-example ledger watch
-request.on('ledger_closed', function(res){
-    console.log('doit?',res);
-});
-*/
-
