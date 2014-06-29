4cb99a30ac71943141e5fe26279591584780abd9
nino brooks <ninobrooks@gmail.com>
2014-06-29 05:58:35 +0200

    cleanup

--- query-api.js
+++ query-api.js
@@ -16,19 +16,26 @@ args ={
 
 //use later during withdrawal testing.
 //client.registerMethod("jsonMethod", "http://remote.site/rest/json/${id}/method", "GET");
+function mcb(data){
+    console.log('endpoints', data.endpoints);
+};
+
+
 client.registerMethod("jsonMethod", "http://localhost:5990/v1", "GET");
 
 //QUERY API
-client.methods.jsonMethod(args,function(data,response){
-    jsData = JSON.parse(data);
-    console.log(jsData);
-    console.log('---------------------\n\n');
-    console.log('endpoints :', jsData.endpoints);
-    console.log('data.endpoints.server_connected :', jsData.endpoints.server_connected);
-});
+function api_query(args, callback){
+    client.methods.jsonMethod(args,function(data,response){
+        jsData = JSON.parse(data);
+        console.log('endpoints :', jsData.endpoints);
+        console.log('data.endpoints.server_connected :', jsData.endpoints.server_connected);
+        callback(jsData);
+    });
+};
 
+//console.log ('api_res>>', api_res.endpoints);
 
-
+xx = api_query(args, mcb);
 //client.get("http://remote.site/rest/json/${id}/method?arg1=hello&arg2=world", args, 
 /*client.get("http://localhost:5990/v1", args, 
 
