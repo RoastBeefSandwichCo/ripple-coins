32f1538d075741c2b102bdd8e678eada33efab94
nino brooks <ninobrooks@gmail.com>
2014-06-30 12:24:53 +0200

    parse cryptocurrencies, create bitcoin client connections

--- process-withdrawal.js
+++ process-withdrawal.js
@@ -1,45 +1,70 @@
-https://www.npmjs.org/package/bitcoin
-require ("bitcoin");
+//https://www.npmjs.org/package/bitcoin
+var bitcoin = require ('bitcoin');
 //note: note all commands are supported by all bitcoin clients, but there is enough consistency
 //for our purposes and this library allows us to offer extensibility to future maintainers
 
 var coins = require ("./cryptocurrencies.json"); //may need to JSON.parse(coins)
-
+console.log(coins);
+console.log(coins.PHC.port);
 //external call instantiates
 var coinDaemons = {};
+//in constructor, open connections to all coin daemons
+
+for (var each in coins){
+console.log(each);
+}
+console.log('------');
+for (var each in coins){ //no idea if this mapping attempt with work
+console.log ('each',each);
+//Should scale to higher loads better than connecting for every tx
+//for each object (coin configuration) in the
+        coinDaemons[each]/* where each is name of object. i.e., CR1, CR2, PHC)*/ = new bitcoin.Client({ //map property names to variable names to call by name later
+        host: 'localhost',
+        port: each.port,
+        user: each.rpcusername,
+        pass: each.rpcpassword,
+        timeout: 30000
+    });
+console.log('port',coins[each].port);
+
+}
+
+function selfTest(){ //selfTest is BROKEN!
+    console.log('selfTest!');
+    for(var obj in coins){
+        console.log(obj + ': ' + coins[obj]);
+        for (var propt in obj){
+//            if (obj.hasOwnProperty(propt)) {
+        // do stuff
+            console.log(propt, obj[propt]);
+//            }
+//            console.log(propt + ': ' + obj[propt]);
+        }
+    }
+}
 
 function coinProcessing(transaction){
-//in constructor, open connections to all coin daemons
-for (var each in coins){ //no idea if this mapping attempt with work
-//Should scale to higher loads better than connecting for every tx
-    var coinDaemomns[each]/* where each is name of object. i.e., CR1, CR2, PHC)*/ = new bitcoin.Client({ //map property names to variable names to call by name later
-      host: 'localhost',
-      port: coins.each.port,
-      user: coins.each.rpcusername,
-      pass: coins.each.rpcpassword,
-      timeout: 30000
-    });
 
-withdrawals is AN ARRAY. so like, handle that.
+//withdrawals is AN ARRAY. so like, handle that.
     function sendTx(){
     //send transaction
-        should look something like:
-        command                      address                                     amount                                                     comment
-        propertyname.sendToAddress  transaction.withdrawals.external_account_id transacton.withdrawals.(convertToNumber)amount "'ripple_transaction_id'"
-        coinDaemons[transaction.withdrawals.currency].sendToAddress(transaction.withdrawals.external_account_id, transaction.withdrawals.(convertToNumber)amount, "'ripple_transaction_id'", function(err, txid, resHeaders){
+//        should look something like:
+//        command                      address                                     amount                                                     comment
+//        propertyname.sendToAddress  transaction.withdrawals.external_account_id transacton.withdrawals.(convertToNumber)amount "'ripple_transaction_id'"
+/*        coinDaemons[transaction.withdrawals.currency].sendToAddress(transaction.withdrawals.external_account_id, transaction.withdrawals.(convertToNumber)amount, "'ripple_transaction_id'", function(err, txid, resHeaders){
             if (err) return console.log(err);
             console.log('txid:', txid);
-        });
+        });*/
     }
 
-is a txid enough to clear the withdrawal? Hm.
+//is a txid enough to clear the withdrawal? Hm.
 
     function clearWithdrawal(){
         //clear pending_withdrawal
     };
+}
 
-
-
+abc = selfTest();
 
 //withdrawal object example
 //http://github.com/ripple/gatewayd#listing-withdrawals
