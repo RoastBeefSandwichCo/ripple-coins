dc2a66e746b90a195418c5153eb51001438060ca
nino brooks <ninobrooks@gmail.com>
2014-07-14 18:42:17 +0200

    added placeholder files

---
+++ coin-daemon-pool.js
@@ -0,0 +1,1 @@
+collection of coin objects goes here from process-withdrawals.js->coinProcessing. Note to self: Fix what you break
---
+++ postgres.js
@@ -0,0 +1,1 @@
+placeholder. This module will export the database object for read-write
