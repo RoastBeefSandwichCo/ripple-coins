57be4044ac2a462d04ad8a05217b74307e30a7b0
jimster6543 <jake.mcshane@comcast.net>
2014-08-29 23:30:32 -0400

    Moved code from process-withdrawal.js to outgoing-bridge.js

--- process-withdrawal.js
+++ process-withdrawal.js
@@ -4,10 +4,10 @@
 var bitcoin = require ('node-dogecoin'); //https://www.npmjs.org/package/bitcoin greatly simplifies interfacing with coin daemons
 var coins = require ("./cryptocurrencies.json");//coin configurations
 var coinDaemons = require ("coin-daemons.js";)
+var transactions = require ("./lib/outgoing-bridge.js")
 //var coinDaemons = {};
 sep = '\n--------------------------------------------------------\n';
 logPrefix = 'process-withdrawal';
-
 //keeping this around since it also shows rpc methods available
 function showCollection(objCollection){//shows properties of objects in a collection. Like port/host/userpass of each coin in cryptocurrencies.json
     console.log(logPrefix, 'selfTest!');
@@ -40,60 +40,6 @@ function selfTest(testLevel, testSelect){ //has not been kept up. Some functions
 }
 
 function coinProcessing(){
-    function sendTx(withdrawalObj, isValid, fnClearPending){
-        console.log(logPrefix, withdrawalObj.external_account_id, 'is valid:',isValid);
-        if (isValid != true){
-            console.log(logPrefix, 'Invalid address. Rejecting.')
-            return false;
-        }
-
-        console.log(sep, logPrefix, 'Processing transaction');
-        amount = Number(withdrawalObj.amount)
-        commentTo = String(withdrawalObj.ripple_transaction_id);
-        console.log(logPrefix, "Converted '" + typeof(withdrawalObj.amount) + "' " + withdrawalObj.amount + " to '" + typeof(amount)+ "' " + amount);
-        console.log(logPrefix, "Converted '" + typeof(withdrawalObj.ripple_transaction_id)  + "' " + withdrawalObj.ripple_transaction_id + " to '" + typeof(commentTo)+ "' " + commentTo);
-        console.log(logPrefix, 'Action:', withdrawalObj.currency, 'sendToAddress(', withdrawalObj.external_account_id, amount, commentTo,')');
-        coinDaemons[withdrawalObj.currency].sendToAddress(withdrawalObj.external_account_id, amount, commentTo, function(err, txid, resHeaders){
-            console.log(logPrefix, 'errors:', err, '\nresHeaders', resHeaders, '\ntxid:', txid);
-            if (txid != undefined ){
-                console.log(logPrefix, 'Transaction sent! TXID:',txid);            
-                fnClearPending(withdrawalObj.id);
-                return true;
-            }else{
-                console.log(logPrefix, 'process-withdrawal>sendTx>Transaction failed. See above.');// not, shit ten bricks
-                return false;
-            }
-        });
-    }
-
-    function validateAddress(thisWithdrawal, fnClearPending){
-        currency = thisWithdrawal.currency;
-        address = thisWithdrawal.external_account_id;
-        errStr = '';
-        resHeaders = '';
-        isValid = '';
-        console.log(sep, logPrefix, 'Validating:',thisWithdrawal.currency, address);
-        coinDaemons[currency].validateAddress(address, function(err, isValid, resHeaders){
-            console.log(logPrefix, 'errors:', err, '\nisValid:', isValid, '\nresHeaders', resHeaders);
-            console.log(logPrefix, address, 'isValid:' , isValid, 'isValid.isvalid:', isValid.isvalid)
-            /*if (err) { console.log(err);}
-            }*/
-
-/*            if (isValid.isvalid == false){// Always returns false, then true (in the same call). Until this is better understood, need protection against double-true
-                console.log(address, ': FALSE!');
-                return false;
-            }*/
-            if (isValid.isvalid == true){
-                console.log(logPrefix, 'VALIDATED');
-                sendTx(thisWithdrawal, isValid.isvalid, fnClearPending);
-                return true;
-            }else{
-                console.log(logPrefix, 'NOT VALIDATED');
-                return false;
-            }
-        });
-    }
-
     function clearWithdrawal(){
         //clear pending_withdrawal
     }
